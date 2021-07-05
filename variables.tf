7a87a543e5b23a7e5d56431288515182a5628fc8
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-05 14:57:31 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,29 +7,35 @@ variable "workspaces" {
 
 variable "key" {
   type = string
+  description = "(Required) Name of the variable."
 }
 
 variable "value" {
   type = string
+  description = "(Required) Value of the variable."
 }
 
 variable "category" {
   type = string
+  description = "(Required) Whether this is a Terraform or environment variable. Valid values are <<terraform>> or <<env>>."
 }
 
 variable "description" {
   type = string
   default = ""
+  description = "(Optional) Description of the variable."
 }
 
 variable "sensitive" {
   type = bool
   default = false
+  description = "(Optional) Whether the value is sensitive. If true then the variable is written once and not visible thereafter. Defaults to false."
 }
 
 variable "hcl" {
   type = bool
   default = false
+  description = "Optional) Whether to evaluate the value of the variable as a string of HCL code. Has no effect for environment variables. Defaults to false."
 }
 
 
