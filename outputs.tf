8300e0b2c22b57fc3de56dd92908d535b5d45373
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-05 16:05:17 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,6 @@
-output "ids" {  
+output "ids" {
+  description = "Return a list of pair workspace_id => variable_id."
+  
   value = { for workspace, variable in tfe_variable.variable:
               variable.workspace_id => variable.id
           }
